ae89855ad9c5060d08592842199b0aecc304b336
Nazmul Hawlader <nazmul.muscled@gmail.com>
2022-06-25 23:50:59 +0600

    errorcheck 005

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -74,7 +74,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Footer />
       </Providers>
 
-{typeof window !== 'undefined'? <PopupWidget
+    {typeof window !== 'undefined'? <PopupWidget
       url="https://calendly.com/muscled/"
       /*
        * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
